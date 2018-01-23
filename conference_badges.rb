44655f9160b7f20e3f70cc0aee123715ae496f02
Kayvan Vakili <kvakili813@gmail.com>
2018-01-23 01:57:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,5 @@ end
 
 def assign_rooms(attendees, room_assignments)
   attendees.each_with_index {|attendees, room|
-}
-  "Hello, #{attendees}! You'll be assigned to room #{room}!"
+  "Hello, #{attendees}! You'll be assigned to room #{room}!"}
 end
