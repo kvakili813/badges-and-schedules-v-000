cad188f2cd4f30894897ae1cca2febe5d08494c5
Kayvan Vakili <kvakili813@gmail.com>
2018-01-23 01:00:54 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do |badge_names|
+  badge_maker(badge_names)
+  end
+end
